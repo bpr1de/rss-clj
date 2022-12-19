3840a4235139c03725c6389fce6468b6bd8e0dfa
Bryan Phillippe <bp@darkforest.org>
2022-12-19 15:02:57 -0800

    Project cleanups.
    - Remove the AOT setting that was blowing up Leiningen.
    - Fix illegal-access warnings with a JVM setting.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject rss "0.1.0-SNAPSHOT"
   :description "RSS article reader with Oracle Notification Service support."
+  :url "https://github.com/bpr1de/rss-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
@@ -9,7 +10,7 @@
                  [com.oracle.oci.sdk/oci-java-sdk-core "3.2.0"]
   ]
   :main rss.core
-  :aot rss.core
   :target-path "target/%s"
+  :jvm-opts ["--illegal-access=deny"]
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
