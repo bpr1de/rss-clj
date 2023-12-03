729eb3ec649b392c699764a85cef89bb43e25ea3
Bryan Phillippe <bp@darkforest.org>
2023-12-02 16:31:35 -0800

    Refactor config parsing; bump to 1.1.0.
    Configuration parsing is now done as part of the runtime loop, which makes
    it possible for config changes to be picked up without restarting the
    application. Furthermore, the configuration path can now be specified by an
    environment variable, allowing for more dynamic execution of the
    application.

--- src/rss/core.clj
+++ src/rss/core.clj
@@ -62,39 +62,49 @@
     )
   )
 
-(defn parse-args
+(defn get-config-path
   [& args]
-  "Parse the command line arguments and return a configuration from it."
+  "Determine a config path from the command-line arguments and environment:
+   1.) if a command-line argument was given, use it; else
+   2.) if the environment variable $RSS_CONFIG_PATH is set, use it; else
+   3.) try the user's own ~/.rss file;"
+  (or (first args)
+      (System/getenv "RSS_CONFIG_PATH")
+      (str (System/getProperty "user.home") "/.rss")
+      )
+  )
+
+(defn read-config
+  [config-path]
+  "Attempt to read the XML config file specified by the path (local or remote)."
   (try
-    ((fnil xml/parse (str (System/getProperty "user.home") "/.rss")) (first args))
+    (println (str "Attempting to read '" config-path "'"))
+    (xml/parse config-path)
     (catch IOException e
-      (do
-        (println (.getMessage e))
-        (System/exit -1)
-        )
+      (println (str "Error reading configuration '" config-path "': " (.getMessage e)))
       )
     )
   )
 
 (defn -main
   [& args]
-  "Read an (optionally specified) XML config file and loop forever over the RSS
-   feeds described in it, publishing each article to a notification client."
-  (let [config (apply parse-args args)
-        notification-client (rss.ons/make-client (get-client-type config))]
-
-    ;; If there are no feeds in the configuration, just exit.
-    (when (nil? (first (get-feeds config)))
-      (println "No feeds in the configuration; exiting.")
-      (System/exit -1)
-      )
+  "Loop forever reading an XML config file and the RSS feeds described in it,
+   publishing each article to the configured notification client."
+  (let [config-path (apply get-config-path args)]
 
     ;; Loop indefinitely, only notifying for new articles.
     (loop [previous-time (Timestamp/valueOf ^LocalDateTime
                               (.. (LocalDateTime/now) (minusDays 7)))
-           sleep-duration 0]
+           sleep-duration 0
+           config (read-config config-path)
+           notification-client (rss.ons/make-client (get-client-type config))]
 
       (Thread/sleep sleep-duration)
+
+      ;; If there are no feeds in the configuration, just skip this cycle.
+      (when (nil? (first (get-feeds config)))
+        (println "Warning: no feeds in the configuration.")
+        )
 
       (doseq [feed (get-feeds config)]
         (try
@@ -114,7 +124,10 @@
         )
 
       (recur (Timestamp/valueOf (LocalDateTime/now))
-             (.. (TimeUnit/MINUTES) (toMillis 10)))
+             (.. (TimeUnit/MINUTES) (toMillis 10))
+             (read-config config-path)
+             (rss.ons/make-client (get-client-type config))
+             )
       )
     )
   )
